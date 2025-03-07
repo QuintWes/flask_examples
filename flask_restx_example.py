1c4933b529a4e7235bcc597ccf4262019a1caa74
quint <quint@sparkles.io>
2025-03-07 11:47:02 +0100

    small update

--- flask_restx_example.py
+++ flask_restx_example.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, Namespace
 
 app = Flask(__name__, template_folder="templates")
-api = Api(app)
+api = Api(app, doc='/docs', title="Pokemon API", version="1.0", description="Simple Pokemon API")
+
+# Create a namespace
+pokemon_ns = Namespace('pokemon', description='Pokemon operations')
 
 @app.route("/")
 def home():
     return render_template("restx_example.html")
 
-@api.route('/api/pokemon/<int:id>')
+# Add route to the namespace
+@pokemon_ns.route('/<int:id>')
 class Pokemon(Resource):
     def get(self, id):
+        """Get a Pokemon by its ID"""
         return {'id': id, 'name': 'Pikachu'}
+
+# Add the namespace to the API
+api.add_namespace(pokemon_ns, path="/api/pokemon")
 
 if __name__ == '__main__':
     app.run(port=5002)
--- flask_swagger_example.py
+++ flask_swagger_example.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, render_template
-from flask_swagger import swagger
+from flasgger import Swagger
+from flask_swagger import swagger as swagger_spec_generator
 
 app = Flask(__name__, template_folder="templates")
+flasgger_swagger = Swagger(app)
 
 @app.route("/")
 def home():
@@ -9,11 +11,28 @@ def home():
 
 @app.route('/api/pokemon/<int:id>')
 def get_pokemon(id):
+    """
+    Get a Pokemon
+    ---
+    parameters:
+      - name: id
+        in: path
+        type: integer
+        required: true
+    responses:
+      200:
+        description: A single Pokemon test
+        examples:
+          application/json: { "id": 1, "name": "Pikachu" }
+    """
     return jsonify({'id': id, 'name': 'Pikachu'})
 
 @app.route("/spec")
 def spec():
-    return jsonify(swagger(app))
+    swag = swagger_spec_generator(app)
+    swag['info']['title'] = "Pokemon API"
+    swag['info']['version'] = "1.0.0"
+    return jsonify(swag)
 
 if __name__ == '__main__':
     app.run(port=5001)
